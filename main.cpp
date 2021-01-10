a060218a7ac2d2745df30fe35614619e0fa6d793
cvalerio <cvalerio723@gmail.com>
2021-01-09 20:56:45 -0500

    cleaned up source code

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
  * main.cpp
  *
  *  Created on: Mar 20, 2020
- *      Author: Christopher
+ *  Author: Christopher
  */
-
 
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-class runLength {
+class runLength
+{
 public:
 	int numRows;
 	int numCols;
@@ -19,7 +19,8 @@ public:
 	int maxVal;
 	int whichMethod;
 	string nameEncodeFile;
-	runLength() {
+	runLength()
+	{
 		numRows = -1;
 		numCols = -1;
 		minVal = -1;
@@ -28,86 +29,85 @@ public:
 		nameEncodeFile = "";
 	}
 
-
-
-	void encodeMethod1(ifstream& in, ofstream& encode) { // encode zeros and no wrap around
+	void encodeMethod1(ifstream &in, ofstream &encode) // encodes zeros and does not implement wrap around
+	{ 
 		string numRows, numCols, minVal, maxVal, curVal, nextVal;
 
-		if(in.is_open() && !in.eof()) {
+		if (in.is_open() && !in.eof())
+		{
 			in >> numRows;
 			in >> numCols;
 			in >> minVal;
 			in >> maxVal;
-			encode << numRows << " " << numCols  << " " << minVal << " " << maxVal << endl;
+			encode << numRows << " " << numCols << " " << minVal << " " << maxVal << endl;
 			int row = 0;
-			while(row < stoi(numRows)) {
+			while (row < stoi(numRows))
+			{
 
 				int col = 0;
 				int length = 1;
 				in >> curVal;
 				encode << row << " " << col << " " << curVal << " ";
 
-				while(col < stoi(numCols)-1) {
+				while (col < stoi(numCols) - 1)
+				{
 					col++;
 
 					in >> nextVal;
-					if(stoi(nextVal) == stoi(curVal) && (col-1 < stoi(numCols))) {
+					if (stoi(nextVal) == stoi(curVal) && (col - 1 < stoi(numCols)))
+					{
 						length++;
 					}
-					else {
+					else
+					{
 						encode << length << endl;
 						curVal = nextVal;
 						length = 1;
 						encode << row << " " << col << " " << curVal << " ";
 					}
-
 				}
-				encode << length  << endl;
+				encode << length << endl;
 				row++;
 			}
 		}
 	}
 
+	void encodeMethod4(ifstream &in, ofstream &encode) // dont encode zeroes and implement wrap around
+	{
 
-
-	void encodeMethod4(ifstream& in, ofstream& encode) {
-
-
-		if(!in.eof()) {
+		if (!in.eof())
+		{
 			string numRows, numCols, minVal, maxVal, curVal, nextVal;
-			in >> numRows; this->numRows = stoi(numRows);
-			in >> numCols; this->numCols = stoi(numCols);
-			in >> minVal; this->minVal = stoi(minVal);
-			in >> maxVal; this->maxVal = stoi(maxVal);
-			encode << numRows << " " << numCols  << " " << minVal << " " << maxVal << endl;
-
+			in >> numRows;
+			this->numRows = stoi(numRows);
+			in >> numCols;
+			this->numCols = stoi(numCols);
+			in >> minVal;
+			this->minVal = stoi(minVal);
+			in >> maxVal;
+			this->maxVal = stoi(maxVal);
+			encode << numRows << " " << numCols << " " << minVal << " " << maxVal << endl;
 
 			int zeroCnt = 0;
 			int row = 0;
 			int col = 0;
-			int length  = 0;
+			int length = 0;
 			int lastVal = skipZeros(in, row, col, zeroCnt);
 			length++;
 			encode << endl;
 			encode << row << " " << col << " " << lastVal << " ";
-			while(!in.eof()) {
-				//encode << row << " " << col << " " << lastVal << " ";
-				//length++;
+			while (!in.eof())
+			{
 				int nextVal = skipZeros(in, row, col, zeroCnt);
-				//encode <<  row << " " << col << " " << nextVal << " ";
 
-				if(zeroCnt > 0) {
-					//encode << row << " " << col << " " << lastVal << " ";
-					//lastVal = 0;
-
-				}
-
-				if(nextVal == lastVal) {
+				if (nextVal == lastVal)
+				{
 					length++;
 					lastVal = nextVal;
 				}
-				else {
-					encode << length <<  endl;
+				else
+				{
+					encode << length << endl;
 					encode.flush();
 
 					lastVal = nextVal;
@@ -116,7 +116,6 @@ public:
 					encode.flush();
 				}
 				//lastVal = nextVal;
-
 			}
 			encode << length << endl;
 			encode.flush();
@@ -125,107 +124,91 @@ public:
 		encode.close();
 	}
 
-
-	int skipZeros(ifstream& in, int& row, int& col, int& zeroCnt) {
+	int skipZeros(ifstream &in, int &row, int &col, int &zeroCnt)
+	{
 
 		string curVal;
 		string pixelVal;
 		zeroCnt = 0;
-		if(row == 0 && col == 0) {
-
+		if (row == 0 && col == 0)
+		{
 		}
 		else
 			col++;
 		in >> curVal;
 
-		if(col >= this->numCols) {
+		if (col >= this->numCols)
+		{
 			col = 0;
 			row++;
 		}
-		while(stoi(curVal) == 0 && !in.eof()) {
-
+		while (stoi(curVal) == 0 && !in.eof())
+		{
 
 			col++;
 			in >> curVal;
 
-			if(stoi(curVal) == 0)
+			if (stoi(curVal) == 0)
 				zeroCnt = zeroCnt + 1;
 
-			if(col >= this->numCols) {
+			if (col >= this->numCols)
+			{
 				col = 0;
 				row++;
 			}
-			if(stoi(curVal) != 0){
+			if (stoi(curVal) != 0)
+			{
 				pixelVal = curVal;
 			}
-
-
 		}
-		if(stoi(curVal) != 0){
+		if (stoi(curVal) != 0)
+		{
 			pixelVal = curVal;
 		}
 		return stoi(pixelVal);
-
 	}
-
 };
 
+int main(int argc, char *argv[])
+{
 
-
-int main(int argc, char* argv[]) {
-	cout << argc << endl;
-	for(int i = 1; i < argc; i++) {
-		cout << argv[i] << " ";
-	}
-	ifstream inFile; ofstream outFile;
+	ifstream inFile;
+	ofstream outFile;
 	inFile.open(argv[1]);
 	outFile.open(argv[3]);
 	string numRows, numCols, minVal, maxVal;
 
-
-	runLength* code = new runLength();
+	runLength *code = new runLength();
 	string whichMethod = argv[2];
 	string nameEncodeFile = argv[1];
-	cout << nameEncodeFile << endl;
-	cout << endl << nameEncodeFile;
-	//nameEncodeFile.append(".txt");
-	//ofstream encodeFile; encodeFile.open(nameEncodeFile.c_str());
-	//encodeFile << numRows << " " << numCols  << " " << minVal << " " << maxVal << endl;
-	//encodeFile << nameEncodeFile << "Method"<< whichMethod << endl;
+
 	int whichM = stoi(whichMethod);
 	string test;
-	test.append(nameEncodeFile.substr(0 ,nameEncodeFile.find_first_of(".", 0)));
+	test.append(nameEncodeFile.substr(0, nameEncodeFile.find_first_of(".", 0)));
 	test.append("_EncodeMethod" + whichMethod + ".txt");
-	cout << endl << "test: " << test << endl;
+
 	ofstream encodeFile;
-	//encodeFile << test << endl;
-	//encodeFile << "File: " << nameEncodeFile << " Method: "<< whichM << endl;
-	//encodeFile << numRows << " " << numCols  << " " << minVal << " " << maxVal << endl;
-	switch(whichM) {
+
+	switch (whichM)
+	{
 	case 1:
 		cout << "method 1";
 		encodeFile.open(test.c_str());
-		encodeFile << "File: " << nameEncodeFile << " Method: "<< whichM << endl;
+		encodeFile << "File: " << nameEncodeFile << " Method: " << whichM << endl;
 		code->encodeMethod1(inFile, encodeFile);
 		break;
 
 	case 4:
 		cout << "method 4";
 		encodeFile.open(test.c_str());
-		encodeFile << "File: " << nameEncodeFile << " Method: "<< whichM << endl;
+		encodeFile << "File: " << nameEncodeFile << " Method: " << whichM << endl;
 		code->encodeMethod4(inFile, encodeFile);
 		break;
 
 	default:
 		cout << "Wrong Input";
-
 	}
 	inFile.close();
 	encodeFile.close();
 	return 0;
 }
-
-
-
-
-
